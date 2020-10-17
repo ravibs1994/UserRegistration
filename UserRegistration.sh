49b1a2a563c6a5c9f7a55391e04c2e9a558c79c1
unknown <ravibs1994@gmail.com>
2020-10-17 16:41:33 +0530

    UC2 Last Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+#First Name
 read -p "Enter First Name " name
 pat='^[A-Z]{1}[a-z]{2,}$'
 if [[ $pat =~ $name ]]
@@ -7,4 +8,13 @@ if [[ $pat =~ $name ]]
     else
 	echo "Invalid"
 fi
+#Last Name
+read -p "Enter Last Name " lName
+if [[ $pat =~ $lName ]]
+    then
+        echo "Valid"
+    else
+        echo "Invalid"
+fi
+
 
